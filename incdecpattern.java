1b1bc463ac37448061d6a84f338e9c77c888a3c4
Janardhan9392 <112267368+Janardhan9392@users.noreply.github.com>
2023-03-23 09:53:53 +0530

    Add files via upload

---
+++ incdecpattern.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+class pattern
+{
+ public static void main(String args[])
+{
+ int r,c,i,j;
+ Scanner s=new Scanner(System.in);
+ System.out.println("enter the row");
+ r=s.nextInt();
+ for(i=1;i<=r;i++)
+{
+ for(j=1;j<=i;j++)
+  {
+   System.out.print(i+" ");
+  }
+  System.out.println();
+  if(i==r)
+       break;
+ }
+   for(i=r-1;i>=1;i--)
+  {
+   for(j=1;j<=i;j++)
+ {
+  System.out.print(i+" ");
+ }
+ System.out.println();
+}
+
+}
+}
